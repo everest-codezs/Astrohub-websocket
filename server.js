3cf8cd491ad7711c356baab86ec5864cb5b913d1
Everest <K11903053@gmail.com>
2025-12-10 02:51:13 +0000

    Initial WebSocket server

---
+++ server.js
@@ -0,0 +1,16 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    console.log("Received:", message);
+    ws.send("Server says: " + message);
+  });
+
+  ws.send("Hello! Welcome to the WebSocket server.");
+});
+
+console.log("WebSocket server running on ws://localhost:8080");
